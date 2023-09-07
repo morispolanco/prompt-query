d078259c087eaf9d20234c8d423e35226c1d560c
morispolanco <morispolanco@gmail.com>
2023-09-07 08:53:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,44 @@
-import streamlit as st
+Copyimport streamlit as st
 import pandas as pd
 
-# Cargar el archivo de Excel usando Streamlit
-uploaded_file = st.file_uploader("Cargar archivo de Excel", type=["xlsx"])
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
+def excel_to_database(file_path):
+    # Leer el archivo Excel
+    df = pd.read_excel(file_path)
 
-    # Mostrar el DataFrame en la interfaz de usuario
-    st.write(df)
+    # Crear una base de datos usando SQLite en memoria
+    import sqlite3
+    conn = sqlite3.connect(':memory:')
+    
+    # Guardar el DataFrame en la base de datos como una tabla llamada 'data'
+    df.to_sql('data', conn, index=False)
+    
+    return conn
 
-    # Barra de búsqueda
-    search_term = st.text_input("Buscar")
-    search_results = df[df.astype(str).apply(lambda x: x.str.contains(search_term, case=False)).any(axis=1)]
+def search_database(conn, query):
+    # Ejecutar la consulta en la base de datos
+    results = pd.read_sql_query(query, conn)
+    return results
 
-    # Mostrar los resultados de la búsqueda
-    st.write("Resultados de la búsqueda:")
-    st.write(search_results)
+# Configurar la aplicación Streamlit
+st.title("Hoja de Excel a Base de Datos")
+st.write("Cargue un archivo de Excel para convertirlo en una base de datos searchable")
+
+# Cargar el archivo de Excel
+file = st.file_uploader("Cargar archivo de Excel", type=["xls", "xlsx"])
+
+if file:
+    conn = excel_to_database(file)
+    st.write("Hoja de Excel cargada correctamente")
+    
+    # Ingrese la consulta de búsqueda
+    query = st.text_input("Ingrese una consulta de búsqueda")
+    
+    if st.button("Buscar"):
+        results = search_database(conn, query)
+        
+        # Mostrar los resultados de la búsqueda
+        if len(results) > 0:
+            st.write("Resultados de búsqueda:")
+            st.dataframe(results)
+        else:
+            st.write("No se encontraron resultados")
